b2c49a8db157fbb747932414c555d17a351994da
김호연 <sgm00006@gmail.com>
2025-04-24 15:28:32 +0900

    재고 감소 로직에 대한 테스트케이스 작성
    동시에 여러 개의 요청이 온 경우 재고가 정상적으로 갱신되는지 확인하기 위하여 멀티쓰레드 활용한 테스트 로직 작성

--- stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -40,7 +44,9 @@ class StockServiceTest {
     }
 
     /**
-     * Test 1-1) 재고 감소 로직 테스트
+     * Test 1-1) 재고 감소 로직 단 건 (1개의 요청) 테스트
+     * - 로직은 수행될 수 있으나 발생 가능한 문제점이 있음.
+     *   . 만약에 동시에 여러개의 요청이 들어온다면..?
      */
     @Test
     public void decreaseStockQuantityTest() {
@@ -52,5 +58,53 @@ class StockServiceTest {
 
         // 실제 데이터 확인 (왼쪽 파라미터 : 기대값 99 / 오른쪽 파라미터 : 실제값 ??)
         assertEquals(99L, stock.getQuantity());
+    }
+
+    /**
+     * Test 1-2) 재고 감소 로직 동시성 (동시에 여러 건의 요청) 테스트
+     * - 특정 상품의 재고가 100개이고 100건의 요청이 동시에 들어온다고 가정한 테스트케이스
+     *  . 기대하는 값은 재고가 0 이어야 하지만 실제로 동작한 결과 96건의 재고가 확인됨
+     * - Race Condition 이 발생했기 때문
+     *  . Race Condition 이란 둘 이상의 쓰레드가 공유 데이터(Stock Entity)에 엑세스 할 수 있고
+     *    동시에 변경을 하려고 할 때 발생하는 문제로
+     *    A 쓰레드가 특정 상품의 재고가 100개 있는걸 확인하고 재고를 변경하기 전에
+     *    B 쓰레드가 특정 상품의 재고를 확인하게 됨. A 쓰레드가 재고를 변경하기 전에 먼저 접근해버리는 바람에 B 쓰레드도 재고 100개로 확인함.
+     *    결국 A 쓰레드도 100 - 1 = 99 개로 갱신하고 B 쓰레드도 100 - 1 = 99 개로 갱신 해버림.
+     * - 해결방안
+     *  . 하나의 쓰레드가 작업이 완료된 이 후에 다른 쓰레드가 데이터에 접근하도록 하면 될 것으로 확인.
+     */
+    @Test
+    public void decreaseStockQuantityConcurrencyTest() throws InterruptedException {
+        // 동시에 여러개의 요청을 보내야 하기 때문에 멀티쓰레드 사용하여 100개의 요청을 보낼 것
+        int threadCount = 100;
+
+        // 멀티쓰레드 사용을 위한 ExecutorService 사용 (비동기로 실행하는 작업을 단순화 하여 사용할 수 있게 도와주는 자바 API)
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        /*
+         * 100 건의 요청이 모두 끝날때 까지 기다려야 하므로 CountDownLatch 사용
+         * CountDownLatch : 다른 쓰레드에서 수행중인 작업이 완료될 때 까지 대기할 수 있도록 도와주는 클래스
+         */
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // 100 건 요청 수행 로직
+        for(int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        // 상품 재고 조회
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 실제 데이터 확인 (왼쪽 파라미터 : 기대값 0 / 오른쪽 파라미터 : 실제값 ??)
+        assertEquals(0, stock.getQuantity());
+
     }
 }
